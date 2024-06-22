5bb07bab4677df84646edb495b1e62e22f0a8acd
DeadlineEm <jownsall@gmail.com>
2024-06-22 11:35:48 -0400

    Update update.bat
    Added YimASI + ScriptHookV downloads to Optional Downloads

--- update.bat
+++ update.bat
@@ -14,13 +14,13 @@ if errorlevel 1 (
     echo Update found! Updating your update.bat to the latest version...
     move /y "%scriptFolder%update.bat.new" "%scriptFolder%update.bat" >nul
     echo Script updated successfully!
-	echo "Returning to the main menu in 5 seconds."
-	timeout /t 5 /nobreak >nul
+    echo "Returning to the main menu in 5 seconds."
+    timeout /t 5 /nobreak >nul
 ) else (
     echo No updates found.
     del "%scriptFolder%update.bat.new" >nul
-	echo "Returning to the main menu in 5 seconds."
-	timeout /t 5 /nobreak >nul
+    echo "Returning to the main menu in 5 seconds."
+    timeout /t 5 /nobreak >nul
 )
 :: Continue with the main script
 goto menu
@@ -46,7 +46,7 @@ echo "                                                               "
 echo "                Extras Addon for YimMenu                       "
 echo "                  Addon Version: 1.0.9                         "
 echo "    https://github.com/Deadlineem/Extras-Addon-for-YimMenu     "
-echo "    		http://extrasaddon.us.to/      		     "
+echo "            http://extrasaddon.us.to/                          "
 echo "  ______   ______   ______   ______   ______   ______   ______ "
 echo " /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ "
 
@@ -70,94 +70,40 @@ if not exist "%destinationFolder3%" (
 )
 echo ------------------------------------------------------------------
 echo   This app is for easily Downloading/Updating Yim/Extras Addon
-echo 	  and tools you may want/need for YimMenu itself.
+echo       and tools you may want/need for YimMenu itself.
 echo ------------------------------------------------------------------
 echo ------------------------------------------------------------------
-echo 			Main Menu
+echo             Main Menu
 echo ------------------------------------------------------------------
 echo Choose an option:
-echo 1. Download Extras Addon (To YimMenu\scripts)
-echo 2. Download YimMenu (To Downloads or Desktop)
+echo 1. Download YimMenu (To Downloads or Desktop)
+echo 2. Download Extras Addon (To YimMenu\scripts)
 echo 3. Download FateInjector (To Downloads or Desktop)
 echo 4. Delete YimMenu Cache Folder (Quick fix when GTA updates)
 echo 5. Optional Downloads
 echo 6. How to install/use YimMenu
-echo 7. Exit the application
+echo 7. Download YimASI (To Downloads or Desktop)
+echo 8. Download ScriptHookV (To Downloads or Desktop)
+echo 9. Exit the application
 echo ------------------------------------------------------------------
 echo If your downloads folder is not in the proper location on your
 echo harddrive, the downloads will default to your desktop, instead.
 
-choice /c 1234567 /n
-if errorlevel 7 goto goodbye
+choice /c 123456789 /n
+if errorlevel 9 goto goodbye
+if errorlevel 8 goto download_scripthookv
+if errorlevel 7 goto download_yimasi
 if errorlevel 6 goto instructions
 if errorlevel 5 goto optional_downloads
 if errorlevel 4 goto delete_cache_folder
 if errorlevel 3 goto download_fate_injector
-if errorlevel 2 goto download_yimmenu
-if errorlevel 1 goto check_yimmenu
+if errorlevel 2 goto download_extras_addon
+if errorlevel 1 goto download_yimmenu
 
-:check_yimmenu
-    set /p yn=Have you downloaded/used YimMenu before? (Y/N): 
-    if /i "%yn%"=="Y" (
-        goto download_extras_addon
-    ) else (
-		cls
-        echo "Please download and use YimMenu first before installing the Extras Addon."
-        echo "After using YimMenu, you can come back and install the Extras Addon."
-        echo "to use YimMenu, download an injector like FateInjector, Xenos or ProcessHacker2."
-		echo ------------------------------------------------------------------
-		echo "When running YimMenu for the first time, click Update Cache and load into story mode or online."
-		echo "If you don't know how to use YimMenu, press 6 on the main menu for instructions"
-		echo ------------------------------------------------------------------
-		echo "Returning to the main menu in 15 seconds."
-		timeout /t 15 /nobreak >nul
-		cls
-		goto menu
-    )
-
-:delete_cache_folder
-echo "Deleting YimMenu's cache folder, this is ONLY necessary if you've updated YimMenu.dll and you're still crashing."
-echo "If this does not fix the issue, check their github issues page at https://github.com/YimMenu/YimMenu/issues"
-rmdir /s /q "%APPDATA%\YimMenu\cache"
-echo "Cache folder deleted successfully."
-echo "Returning to main menu in 10 seconds."
-timeout /t 10 /nobreak >nul
-goto menu
-
-:download_extras_addon
-	cls
-	echo ------------------------------------------------------------------
-	echo 	Downloading Extras Addon from the repository
-	echo ------------------------------------------------------------------
-	echo "Checking to see if there is an existing version of Extras Addon"
-	del "%destinationFolder%\Extras-Addon.lua" >nul 2>&1
-
-	echo "Downloading new version of Extras-Addon.lua from the repository..."
-	set "url=https://raw.githubusercontent.com/Deadlineem/Extras-Addon-for-YimMenu/main/Extras-Addon.lua"
-	set "url2=https://raw.githubusercontent.com/Deadlineem/Extras-Addon-for-YimMenu/main/json.lua"
-	set "url3=https://raw.githubusercontent.com/Deadlineem/Extras-Addon-for-YimMenu/main/Extras-data.lua"
-	powershell -command "& { Invoke-WebRequest -Uri '%url%' -OutFile '%destinationFolder%\Extras-Addon.lua' }"
-	powershell -command "& { Invoke-WebRequest -Uri '%url2%' -OutFile '%destinationFolder%\json.lua' }"
-	powershell -command "& { Invoke-WebRequest -Uri '%url3%' -OutFile '%destinationFolder%\Extras-data.lua' }"
-
-	if not exist "%destinationFolder%\Extras-Addon.lua" (
-		echo "Error: Failed to download Addon. Check the internet connection or the source URL."
-	) else if not exist "%destinationFolder%\json.lua" (
-		echo "Error: Failed to download Json. Check the internet connection or the source URL."
-	) else (
-		echo "Extras Addon downloaded successfully. File Location: %destinationFolder%\Extras-Addon.lua"
-		echo "Json downloaded successfully. (Required json config file) File Location: %destinationFolder%\json.lua"
-		echo "Extras-data downloaded successfully. (Required, stores objects, vehicles, etc.) File Location: %destinationFolder%\json.lua"
-		echo "Returning to the main menu in 10 seconds."
-	)
-	timeout /t 10 /nobreak >nul
-	cls
-	goto menu
-	
 :download_yimmenu
 cls
 echo ------------------------------------------------------------------
-echo 	Downloading YimMenu from the repository
+echo     Downloading YimMenu from the repository
 echo ------------------------------------------------------------------
 echo "Checking to see if there is an existing version of YimMenu"
 del "%destinationFolder2%\YimMenu.dll" >nul 2>&1
@@ -176,14 +122,49 @@ timeout /t 5 /nobreak >nul
 cls
 goto menu
 
-:exit
-echo Exiting...
-exit /b
+:download_extras_addon
+cls
+echo ------------------------------------------------------------------
+echo     Downloading Extras Addon from the repository
+echo ------------------------------------------------------------------
+echo "Checking to see if there is an existing version of Extras Addon"
+del "%destinationFolder%\Extras-Addon.lua" >nul 2>&1
+
+echo "Downloading new version of Extras-Addon.lua from the repository..."
+set "url=https://raw.githubusercontent.com/Deadlineem/Extras-Addon-for-YimMenu/main/Extras-Addon.lua"
+set "url2=https://raw.githubusercontent.com/Deadlineem/Extras-Addon-for-YimMenu/main/json.lua"
+set "url3=https://raw.githubusercontent.com/Deadlineem/Extras-Addon-for-YimMenu/main/Extras-data.lua"
+powershell -command "& { Invoke-WebRequest -Uri '%url%' -OutFile '%destinationFolder%\Extras-Addon.lua' }"
+powershell -command "& { Invoke-WebRequest -Uri '%url2%' -OutFile '%destinationFolder%\json.lua' }"
+powershell -command "& { Invoke-WebRequest -Uri '%url3%' -OutFile '%destinationFolder%\Extras-data.lua' }"
+
+if not exist "%destinationFolder%\Extras-Addon.lua" (
+    echo "Error: Failed to download Addon. Check the internet connection or the source URL."
+) else if not exist "%destinationFolder%\json.lua" (
+    echo "Error: Failed to download Json. Check the internet connection or the source URL."
+) else (
+    echo "Extras Addon downloaded successfully. File Location: %destinationFolder%\Extras-Addon.lua"
+    echo "Json downloaded successfully. (Required json config file) File Location: %destinationFolder%\json.lua"
+    echo "Extras-data downloaded successfully. (Required, stores objects, vehicles, etc.) File Location: %destinationFolder%\json.lua"
+    echo "Returning to the main menu in 10 seconds."
+)
+timeout /t 10 /nobreak >nul
+cls
+goto menu
+
+:delete_cache_folder
+echo "Deleting YimMenu's cache folder, this is ONLY necessary if you've updated YimMenu.dll and you're still crashing."
+echo "If this does not fix the issue, check their github issues page at https://github.com/YimMenu/YimMenu/issues"
+rmdir /s /q "%APPDATA%\YimMenu\cache"
+echo "Cache folder deleted successfully."
+echo "Returning to main menu in 10 seconds."
+timeout /t 10 /nobreak >nul
+goto menu
 
 :download_fate_injector
 cls
 echo ------------------------------------------------------------------
-echo 	Downloading FateInjector from the repository
+echo     Downloading FateInjector from the repository
 echo ------------------------------------------------------------------
 echo "Checking to see if there is an existing version of FateInjector"
 if exist "%destinationFolder3%\FateInjector.exe" (
@@ -209,17 +190,21 @@ goto menu
 :optional_downloads
 cls
 echo ------------------------------------------------------------------
-echo 	Optional Downloads
+echo     Optional Downloads
 echo ------------------------------------------------------------------
 echo Choose an option:
 echo 1. Get XML Maps/Vehicles (Opens in Browser)
 echo 2. Download Animations Dictionary (To YimMenu Root Folder)
-echo 3. Back to Main Menu
+echo 3. Download YimASI (To Downloads Folder)
+echo 4. Download ScriptHookV (To Downloads Folder)
+echo 5. Back to Main Menu
 echo ------------------------------------------------------------------
 echo More optional downloads may be added in the future!
 
-choice /c 1234 /n
-if errorlevel 3 goto menu
+choice /c 12345 /n
+if errorlevel 5 goto menu
+if errorlevel 4 goto download_scripthookv
+if errorlevel 3 goto download_yimasi
 if errorlevel 2 goto download_file_2
 if errorlevel 1 goto download_file_1
 
@@ -233,30 +218,74 @@ goto optional_downloads
 
 :download_file_2
 cls
-	echo ------------------------------------------------------------------
-	echo 	Downloading Animations Dictionary from the repository
-	echo ------------------------------------------------------------------
-	echo "Checking to see if there is an existing version of animDictsCompact.json"
-	del "%APPDATA%\YimMenu\animDictsCompact.json" >nul 2>&1
+echo ------------------------------------------------------------------
+echo     Downloading Animations Dictionary from the repository
+echo ------------------------------------------------------------------
+echo "Checking to see if there is an existing version of animDictsCompact.json"
+del "%APPDATA%\YimMenu\animDictsCompact.json" >nul 2>&1
 
-	echo "Downloading new version of animDictsCompact.json from the repository..."
-	set "url=https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/animDictsCompact.json"
-	powershell -command "& { Invoke-WebRequest -Uri '%url%' -OutFile '%APPDATA%\YimMenu\animDictsCompact.json' }"
+echo "Downloading new version of animDictsCompact.json from the repository..."
+set "url=https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/animDictsCompact.json"
+powershell -command "& { Invoke-WebRequest -Uri '%url%' -OutFile '%APPDATA%\YimMenu\animDictsCompact.json' }"
 
-	if not exist "%APPDATA%\YimMenu\animDictsCompact.json" (
-		echo "Error: Failed to download Animations. Check the internet connection or the source URL."
-	) else (
-		echo "Extras Addon downloaded successfully. File Location: %APPDATA%\YimMenu\animDictsCompact.json"
-		echo "Returning to the main menu in 5 seconds."
-	)
-	timeout /t 5 /nobreak >nul
-	cls
-	goto optional_downloads
+if not exist "%APPDATA%\YimMenu\animDictsCompact.json" (
+    echo "Error: Failed to download Animations. Check the internet connection or the source URL."
+) else (
+    echo "Extras Addon downloaded successfully. File Location: %APPDATA%\YimMenu\animDictsCompact.json"
+    echo "Returning to the main menu in 5 seconds."
+)
+timeout /t 5 /nobreak >nul
+cls
+goto optional_downloads
+
+:download_yimasi
+cls
+echo ------------------------------------------------------------------
+echo     Downloading YimASI from the repository
+echo ------------------------------------------------------------------
+echo "Checking to see if there is an existing version of YimASI"
+del "%destinationFolder3%\WTSAPI32.dll" >nul 2>&1
+
+echo "Downloading new version of YimASI from the repository..."
+set "url=https://github.com/xesdoog/YimASI/releases/download/release/WTSAPI32.dll"
+powershell -command "& { Invoke-WebRequest -Uri '%url%' -OutFile '%destinationFolder3%\WTSAPI32.dll' }"
+
+if not exist "%destinationFolder3%\WTSAPI32.dll" (
+    echo "Error: Failed to download YimASI. Check the internet connection or the source URL."
+) else (
+    echo "YimASI downloaded successfully. File Location: %destinationFolder3%\WTSAPI32.dll"
+    echo "Returning to the main menu in 5 seconds."
+)
+timeout /t 5 /nobreak >nul
+cls
+goto optional_downloads
+
+:download_scripthookv
+cls
+echo ------------------------------------------------------------------
+echo     Downloading ScriptHookV from the repository
+echo ------------------------------------------------------------------
+echo "Checking to see if there is an existing version of ScriptHookV"
+del "%destinationFolder3%\ScriptHookV.zip" >nul 2>&1
+
+echo "Downloading new version of ScriptHookV from the repository..."
+set "url=https://github.com/scripthookvdotnet/scripthookvdotnet-nightly/releases/download/v3.6.0-nightly.88/ScriptHookVDotNet-v3.6.0-nightly.88.zip"
+powershell -command "& { Invoke-WebRequest -Uri '%url%' -OutFile '%destinationFolder3%\ScriptHookV.zip' }"
+
+if not exist "%destinationFolder3%\ScriptHookV.zip" (
+    echo "Error: Failed to download ScriptHookV. Check the internet connection or the source URL."
+) else (
+    echo "ScriptHookV downloaded successfully. File Location: %destinationFolder3%\ScriptHookV.zip"
+    echo "Returning to the main menu in 5 seconds."
+)
+timeout /t 5 /nobreak >nul
+cls
+goto optional_downloads
 
 :instructions
 cls
 echo ------------------------------------------------------------------
-echo 		How to Install/Use YimMenu
+echo         How to Install/Use YimMenu
 echo ------------------------------------------------------------------
 echo "1. Start GTA 5 on your PC"
 echo "2. Open your preferred injector, and find/select YimMenu.dll"
